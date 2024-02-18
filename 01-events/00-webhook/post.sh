b2fb3dc40a61361218592b7991f9832cf31a6e58
chukmunnlee <chukmunnlee@gmail.com>
2024-02-18 14:33:10 +0800

    update post.sh

--- 01-events/00-webhook/post.sh
+++ 01-events/00-webhook/post.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-curl -X POST localhost:12000/notify -H 'Content-Type: application/json' -d '{ "message": { "id": "abc123", "name": "fred", "email": "fred@gmail.com" } }'
+EP="webhook-$(minikube ip).nip.io"
+
+echo $EP
+
+curl -v -X POST ${EP}/notify -H 'Content-Type: application/json' -d '{ "message": { "id": "abc123", "name": "fred", "email": "fred@gmail.com" } }'
