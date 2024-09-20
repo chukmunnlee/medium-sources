a9c88cceca5630a7867fb54286a5e190499f2e1d
Lee Chuk Munn <chukmunnlee@gmail.com>
2024-09-20 16:27:29 +0800

    UPDATE: blue-green workref

---
+++ 02-rollouts/setup/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+k apply -f ./namespace.yaml
+
+echo "Install Argo Rollouts"
+k apply -f ./rollout-install.yaml -nargo-rollouts
+
+echo "Install Argo Rollouts dashboard"
+k apply -f ./dashboard-install.yaml -nargo-rollouts
