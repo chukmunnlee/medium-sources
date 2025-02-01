71c708e528f824647f9610333139904df499aa8f
Lee Chuk Munn <chukmunnlee@gmail.com>
2025-02-01 10:14:24 +0800

    gateway-api with traefik
    Include default and extension filters

--- 02-rollouts/traefik/setup/setup-traefik.sh
+++ 02-rollouts/traefik/setup/setup-traefik.sh
@@ -2,6 +2,7 @@
 k apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml
 k apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3/docs/content/reference/dynamic-configuration/kubernetes-gateway-rbac.yml
 
+
 helm repo update
 
 helm install traefik traefik/traefik -ntraefik --create-namespace -f ./values.yaml
