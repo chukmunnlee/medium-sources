dc06839d2e959ef777aba010af6b5e801be34548
Lee Chuk Munn <chukmunnlee@gmail.com>
2025-04-18 16:12:52 +0800

    UPDATE: canary job analysis with siege

---
+++ 02-rollouts/canary/4-analysis/job/gen-load.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+URL="${1:-http://localhost:3000/api/data}"
+DUR="${2:-30s}"
+EXP_AVAIL="${3:-99}"
+
+echo "DURATION=${DUR}\nURL=${URL}\nEXPECTED_AVAIL=${EXP_AVAIL}"
+
+AVAIL="$(siege -c2 --delay=5 --time=${DUR} ${URL} | jq '.availability')"
+echo "AVAILABILITY = ${AVAIL}"
+IFS="."
+set $AVAIL
+IFS=" "
+if [ "$1" -ge "${EXP_AVAIL}" ]; then
+  exit 0
+else
+  exit 1
+fi
+#if [ $(echo "$AVAIL >= 99.00" | bc) -eq 1 ]; then
+#  exit 0
+#else
+#  exit 1
+#fi
+
