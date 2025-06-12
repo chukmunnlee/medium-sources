7ceee4a859cf6e75ea90c2c90a1e0df67799f3ad
Lee Chuk Munn <chukmunnlee@gmail.com>
2025-06-12 08:45:36 +0800

    UPDATE traefik gateway version

--- 02-rollouts/traefik/setup/setup-traefik.sh
+++ 02-rollouts/traefik/setup/setup-traefik.sh
@@ -1,7 +1,6 @@
 #!/bin/bash 
 k apply -f https://github.com/kubernetes-sigs/gateway-api/releases/download/v1.2.1/standard-install.yaml
-k apply -f https://raw.githubusercontent.com/traefik/traefik/v3.3/docs/content/reference/dynamic-configuration/kubernetes-gateway-rbac.yml
-
+k apply -f https://raw.githubusercontent.com/traefik/traefik/v3.4/docs/content/reference/dynamic-configuration/kubernetes-gateway-rbac.yml
 
 helm repo update
 
